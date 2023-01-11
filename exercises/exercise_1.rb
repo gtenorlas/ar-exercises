ac5c90d28fce96f49b32167b6f1524482c577c7f
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-11 03:49:51 +0000

    Create store records and show table count

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,42 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+=begin
+### Exercise 1: Create 3 stores
+
+1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
+
+- Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+- Richmond (annual_revenue of 1260000 carries women's apparel only)
+- Gastown (annual_revenue of 190000 carries men's apparel only)
+
+2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the database.
+=end
+
 # Your code goes below here ...
+
+#Instantiate the store object without saving until the .save is called
+store = Store.new
+store.name = "Burnaby"
+store.annual_revenue = 300000
+store.mens_apparel = true
+store.womens_apparel = true
+store.save
+
+#Save the record right away and stantiate the object
+store2 = Store.create(name: "Richmond",annual_revenue:1260000,mens_apparel:false,womens_apparel:true)
+puts store2.name
+
+#object will not instantiated until saved
+store3 = Store.new do |s|
+  s.name="Gastown"
+  s.annual_revenue=190000
+  s.mens_apparel=true
+  s.womens_apparel=false
+end
+store3.save
+
+puts store3.name
+
+
+puts "Store count:", Store.count
