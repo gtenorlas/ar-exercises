e5eec41c327b7019681c9f02ddb940f95ee3d026
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-11 15:45:45 +0000

    Create validations to Employee class

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, inclusion: {in: 40..200}
   belongs_to :store
+  validates_associated: store #validates record is associated to store
 end
